c949f760211169785f4dc4d0196fd09f98f7d43e
shreerang6921 <68148922+shreerang6921@users.noreply.github.com>
2023-08-22 13:50:35 +0530

    removed initial testing code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,8 @@
 import { useState } from "react";
-import PetForm from "./components/PetForm";
-import GetPets from "./components/getPets";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return (
-    <>
-      <PetForm />
-      <GetPets />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
